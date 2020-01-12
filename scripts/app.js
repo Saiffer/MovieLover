b6d5ceee129001da2153c8d3d9ca712360b991d8
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-01-12 14:57:51 -0500

    All pages
    Added possibility to pull movie data from all pages

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 6 */
 let submitBtn = document.querySelector('.submit-search-movie');
 let userInput = document.querySelector('.search-movie');
-
+let pageBtn = document.querySelectorAll('.page-number');
 
 let movie;
 
@@ -17,38 +17,64 @@ function getMovie() {
         .then(function(data){
             movie = data;
             console.log(movie);
+            
             changeMovies();
         });   
 }
+
 
 function fetchAllMovie() {
 
    for(let i = 0; i < movie.results.length; i++) {
         let div = document.getElementById('movie-append');
         let divInner = document.createElement('div');
-        divInner.classList.add('card');
         let parag = document.createElement('p');
-        parag.classList.add('movieTitle');
         let movieTitle = movie.results[i].title;
         let movieReleaseDate = movie.results[i].release_date;
-        parag.textContent = movieTitle + '' + movieReleaseDate;
         let posterPath = movie.results[i].poster_path;
         let imgPoster = document.createElement('img');
-        let imgPath = `http://image.tmdb.org/t/p/w185/${posterPath}`;
+        let imgPath = `http://image.tmdb.org/t/p/w200/${posterPath}`;
+
+        divInner.classList.add('card');
+        parag.classList.add('movieTitle');
+        parag.textContent = `${movieTitle} ${movieReleaseDate}`;
         imgPoster.src = imgPath;
+
         divInner.appendChild(parag);
         divInner.appendChild(imgPoster);
         div.appendChild(divInner);
+
         console.log(parag);
-   }
         console.log(div);
+   }
 }
 
 function changeMovies() {
     let div = document.getElementById('movie-append');
     div.innerHTML = '';
-    fetchAllMovie();
+    tryGetMoreMovies();
 }
+
+function tryGetMoreMovies() {
+    let api; let valueOfInput; let pageCount
+    for(let i = 1; i <= movie.total_pages; i++) {
+        valueOfInput = userInput.value;
+        pageCount = i;
+        api = `https://api.themoviedb.org/3/search/movie?api_key=05c32981e48de1e61d5e174e4f052daa&language=en-US&query=${valueOfInput}&page=${pageCount}&include_adult=false`;
+        fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data
+        })
+        .then(function(data){
+            movie = data;
+            console.log(movie);
+            fetchAllMovie();
+        }); 
+    }
+    
+}
+
 
 
 submitBtn.addEventListener('click', getMovie);
