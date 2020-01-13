471e72ff9d9cd0dbcf2f0005ef5291bcd7915951
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-01-12 23:47:00 -0500

    Removed API key
    Removed API key and added gitignore file

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,13 @@
 let submitBtn = document.querySelector('.submit-search-movie');
 let userInput = document.querySelector('.search-movie');
 let pageBtn = document.querySelectorAll('.page-number');
-
+let myKey = config.API_KEY;
 let movie;
 
 
 function getMovie() {
     let valueOfInput = userInput.value;
-    let api = `https://api.themoviedb.org/3/search/movie?api_key=05c32981e48de1e61d5e174e4f052daa&language=en-US&query=${valueOfInput}&page=1&include_adult=false`;
+    let api = `https://api.themoviedb.org/3/search/movie?api_key=${myKey}&language=en-US&query=${valueOfInput}&page=1&include_adult=false`;
     fetch(api)
         .then(function(response){
             let data = response.json();
@@ -74,7 +74,7 @@ function nextPage() {
     valueOfInput = userInput.value;
     if(pageCount < movie.total_pages) {
         pageCount++;
-        let api = `https://api.themoviedb.org/3/search/movie?api_key=05c32981e48de1e61d5e174e4f052daa&language=en-US&query=${valueOfInput}&page=${pageCount}&include_adult=false`;
+        let api = `https://api.themoviedb.org/3/search/movie?api_key=${myKey}&language=en-US&query=${valueOfInput}&page=${pageCount}&include_adult=false`;
         fetch(api)
     .then(function(response){
         let data = response.json();
@@ -93,7 +93,7 @@ function previousPage() {
     valueOfInput = userInput.value;
     if(pageCount > 1) {
         pageCount--;
-        let api = `https://api.themoviedb.org/3/search/movie?api_key=05c32981e48de1e61d5e174e4f052daa&language=en-US&query=${valueOfInput}&page=${pageCount}&include_adult=false`;
+        let api = `https://api.themoviedb.org/3/search/movie?api_key=${myKey}&language=en-US&query=${valueOfInput}&page=${pageCount}&include_adult=false`;
         fetch(api)
     .then(function(response){
         let data = response.json();
